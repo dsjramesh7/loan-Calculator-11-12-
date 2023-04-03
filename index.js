d89de87b917764b206b76949b46ac996bd2d3f0a
Ramesh <rameshdussa07@gmail.com>
2023-04-03 13:27:53 +0530

    changes in html and js done with project

---
+++ index.js
@@ -0,0 +1,15 @@
+function calculateLoan() {
+  loanAmountValue = document.getElementById("lAmount").value;
+
+  interestRateValue = document.getElementById("interest").value;
+
+  MonthsToPayValue = document.getElementById("months").value;
+
+  interest = (loanAmountValue * (interestRateValue * 0.01)) / MonthsToPayValue;
+
+  monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2);
+
+  document.getElementById(
+    "payment"
+  ).innerHTML = `Monthly Payment: ${monthlyPayment}`;
+}
